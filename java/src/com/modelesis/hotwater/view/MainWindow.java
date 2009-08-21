fe7834a2797f5b0c8b9da15389ee8de069c2c306
ramon <ramon@cfe0fdc5-92af-314e-908d-82f65cf30e90>
2009-08-21 15:48:29 +0000

    Allow one-click navigation to 'morning', 'afternoon' and 'evening' segments of schedule table.

--- java/src/com/modelesis/hotwater/view/MainWindow.java
+++ java/src/com/modelesis/hotwater/view/MainWindow.java
@@ -5,11 +5,13 @@ package com.modelesis.hotwater.view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -42,16 +44,50 @@ public class MainWindow extends JFrame {
 	private JPanel createMainPanel(JTable scheduleTable) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		panel.add(createButtonPanel(), BorderLayout.NORTH);
+		panel.add(createButtonPanel(scheduleTable), BorderLayout.NORTH);
 		panel.add(new JScrollPane(scheduleTable));
 		return panel;
 	}
 	
-	private JPanel createButtonPanel() {
+	private JPanel createButtonPanel(final JTable scheduleTable) {
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setBorder(BorderFactory.createEtchedBorder());
 		buttonPanel.setLayout(new FlowLayout());
-		final JButton btnUndo = new JButton("Undo");
+
+		final JButton btnMorning = new JButton("Mañana");
+		btnMorning.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				vScrollComponent(scheduleTable, 1, 4);
+			}
+		});
+		
+		buttonPanel.add(btnMorning);
+		
+		final JButton btnAfternoon = new JButton("Tarde");
+		btnAfternoon.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				vScrollComponent(scheduleTable, 2, 4);
+			}
+		});
+		
+		buttonPanel.add(btnAfternoon);
+
+		final JButton btnEvening = new JButton("Noche");
+		btnEvening.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				vScrollComponent(scheduleTable, 3, 4);
+			}
+		});
+		
+		buttonPanel.add(btnEvening);
+
+		final JButton btnUndo = new JButton("Deshacer");
 		btnUndo.setEnabled(controller.canUndo());
 		btnUndo.addActionListener(new ActionListener() {
 
@@ -70,6 +106,23 @@ public class MainWindow extends JFrame {
 		});
 
 		buttonPanel.add(btnUndo);
+		
 		return buttonPanel;
+	}
+	
+	/**
+	 * Logically split a component vertically into n fragments,
+	 * then scroll vertically to the 'position'-th (1-based) fragment.
+	 * 
+	 * @param comp Component to scroll
+	 * @param position Position or fragment to display
+	 * @param nFragments Number of fragments
+	 */
+	private void vScrollComponent(JComponent comp, int position, int nFragments) {
+		Rectangle currentVR = comp.getVisibleRect();
+		int width = currentVR.width;
+		int height = currentVR.height;
+		int y = position * comp.getHeight() / nFragments;
+		comp.scrollRectToVisible(new Rectangle(0, y, width, height));
 	}
 }
